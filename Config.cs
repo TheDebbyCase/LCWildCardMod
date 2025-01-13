29b4fd30690601f1fea2b64c3b2185e025d54d18
TheDebbyCase <thedebbycase@gmail.com>
2025-01-13 00:44:55 +0000

    Initial Commit

---
+++ Config.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using BepInEx.Configuration;
+using HarmonyLib;
+using LethalLib.Modules;
+
+namespace LCWildCardMod
+{
+    public class WildCardConfig
+    {
+        public readonly List<ConfigEntry<bool>> isScrapEnabled = new List<ConfigEntry<bool>>();
+        public readonly List<ConfigEntry<string>> scrapSpawnWeights = new List<ConfigEntry<string>>();
+        public WildCardConfig(ConfigFile cfg, List<Item> scrapList)
+        {
+            cfg.SaveOnConfigSet = false;
+            foreach (Item scrapListItem in scrapList)
+            {
+                isScrapEnabled.Add(cfg.Bind(
+                    "Scrap",
+                    $"Enable {scrapListItem.itemName}",
+                    true,
+                    $"Whether or not to allow {scrapListItem.itemName} to spawn!"
+                    ));
+                scrapSpawnWeights.Add(cfg.Bind(
+                    "Scrap",
+                    $"Spawn Weights of {scrapListItem.itemName}",
+                    "All:20",
+                    string.Concat($"Set the spawn weight of {scrapListItem.itemName} for each moon in the format 'Vanilla:20,Modded:30'",
+                    "\nUse 'All:' to set the weight for all moons, 'Vanilla:' for only vanilla moons, or 'Modded:' for only modded moons.",
+                    "\nSpecific moons can be referenced by typing the name (without the numbers) followed by 'Level' (this works for modded moons also), for example:",
+                    "\n'ExperimentationLevel:20'")
+                    ));
+            }
+            ClearOrphanedEntries(cfg);
+            cfg.Save();
+            cfg.SaveOnConfigSet = true;
+        }
+        static void ClearOrphanedEntries(ConfigFile cfg)
+        {
+            PropertyInfo orphanedEntriesProp = AccessTools.Property(typeof(ConfigFile), "OrphanedEntries");
+            var orphanedEntries = (Dictionary<ConfigDefinition, string>)orphanedEntriesProp.GetValue(cfg);
+            orphanedEntries.Clear();
+        }
+    }
+}
+
---
+++ PluginMain.cs
@@ -0,0 +1,99 @@
+﻿using BepInEx;
+using HarmonyLib;
+using LethalLib.Modules;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+namespace LCWildCardMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class WildCardMod : BaseUnityPlugin
+    {
+        private const string modGUID = "deB.WildCard";
+        private const string modName = "WILD/CARD Things";
+        private const string modVersion = "0.0.1";
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static WildCardMod Instance;
+        internal static WildCardConfig ModConfig {get; private set;} = null!;
+        private readonly string[] declaredAssetPaths = {"assets/my creations/items", "assets/my creations/enemies"};
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            AssetBundle bundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "wildcardmod"));
+            List<Item> scrapList = new List<Item>();
+            foreach (string assetPath in bundle.GetAllAssetNames())
+            {
+                if (declaredAssetPaths.Contains(assetPath[..assetPath.LastIndexOf("/")]))
+                {
+                    scrapList.Add(bundle.LoadAsset<Item>(assetPath));
+                }
+                else
+                {
+                    Logger.LogDebug($"\"{assetPath}\" is not a known asset path, skipping.");
+                }
+            }
+            ModConfig = new WildCardConfig(base.Config, scrapList);
+            for (int i = 0; i < scrapList.Count; i++)
+            {
+                if (ModConfig.isScrapEnabled[i].Value)
+                {
+                    Dictionary<Levels.LevelTypes, int> scrapLevelWeights = new Dictionary<Levels.LevelTypes, int>();
+                    Dictionary<string, int> scrapModdedWeights = new Dictionary<string, int>();
+                    string[] configScrapStringArray = ModConfig.scrapSpawnWeights[i].Value.Split(",");
+                    bool isScrapConfigStringValid = new bool();
+                    foreach (string configScrapStringCheck in configScrapStringArray)
+                    {
+                        if ((configScrapStringCheck.Contains(":") && int.TryParse(configScrapStringCheck.Split(":")[1], out _)))
+                        {
+                            isScrapConfigStringValid = true;
+                        }
+                        else
+                        {
+                            isScrapConfigStringValid = false;
+                            break;
+                        }
+                    }
+                    if (isScrapConfigStringValid)
+                    {
+                        foreach (string configScrapString in configScrapStringArray)
+                        {
+                            if (Levels.LevelTypes.TryParse(configScrapString.Split(":")[0], out Levels.LevelTypes configScrapLevel))
+                            {
+                                scrapLevelWeights.Add(configScrapLevel, int.Parse(configScrapString.Split(":")[1]));
+                            }
+                            else
+                            {
+                                scrapModdedWeights.Add(configScrapString, int.Parse(configScrapString.Split(":")[1]));
+                            }
+                        }
+                        LethalLib.Modules.Items.RegisterScrap(scrapList[i], null, scrapModdedWeights);
+                        LethalLib.Modules.Items.RegisterScrap(scrapList[i], scrapLevelWeights);
+                        NetworkPrefabs.RegisterNetworkPrefab(scrapList[i].spawnPrefab);
+                        Utilities.FixMixerGroups(scrapList[i].spawnPrefab);
+                        foreach (KeyValuePair<Levels.LevelTypes, int> debugRarities in Items.scrapItems.LastOrDefault().levelRarities)
+                        {
+                            Logger.LogDebug($"LethalLib Registered Weights {debugRarities}");
+                        }
+                        Logger.LogInfo($"{scrapList[i].itemName} was loaded!");
+                    }
+                    else
+                    {
+                        Logger.LogWarning($"{scrapList[i].itemName} was not loaded as its config was not set up correctly!");
+                    }
+                }
+                else
+                {
+                    Logger.LogInfo($"{scrapList[i].itemName} was disabled!");
+                }
+            }
+            Logger.LogInfo("WILD/CARD Things Successfully Loaded");
+        }
+    }
+}
